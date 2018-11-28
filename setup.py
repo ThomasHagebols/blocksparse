d1fe22e34a85ff3adf8af7bf6ef9ccd6f5cf6675
Thomas Hagebols <ThomasHagebols@users.noreply.github.com>
2018-11-28 15:16:35 +0100

    Missing networkx dependency

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     install_requires=[
         'numpy',
         'scipy',
+        'networkx',
         # We don't depend on `tensorflow` or `tensorflow-gpu` here, since one or the other is sufficient.
     ],
     packages=['blocksparse'],
